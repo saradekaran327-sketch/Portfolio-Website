c4c9dedc9428625aafd2c3c5219943f1ef095f2b
saradekaran327-sketch <saradekaran327@gmail.com>
2026-07-04 20:00:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+    // Loader
+    window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 600);
+
+});
+
+// Typing Animation
+
+new Typed(".typing",{
+
+strings:[
+"C Programmer",
+"HTML Learner",
+"Future Full Stack Developer",
+"Web Developer"
+],
+
+typeSpeed:80,
+backSpeed:50,
+backDelay:1500,
+loop:true
+
+});
+
+// AOS
+
+AOS.init({
+
+duration:1000,
+once:true
+
+});
